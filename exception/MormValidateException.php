944ed23ae5db1ee0b95f1a33a0a02c1c33051474
François de Metz <fdemetz@af83.com>
2009-09-25 15:49:14 +0200

    fixed an unusable exception ...

--- exception/MormValidateException.php
+++ exception/MormValidateException.php
@@ -2,20 +2,12 @@
 
 class MormValidateException extends MormSqlException
 {
-    public $errors = array();
-
-     public function __construct($message = 'Erreur de validation')
-     {
-         if(is_array($message)) 
-          {
-              $this->message = print_r($message,TRUE);
-              $this->errors = $message;
-          } 
-          else 
-          {
-              $this->message = $message;
-          }
-     }
-     
-
+    public function __construct($message = 'Erreur de validation')
+    {
+        if(!is_array($message)) 
+        {
+            $message = array($message);
+        } 
+        $this->message = $message;
+    }
 }
